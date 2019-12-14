42a77227c48ae5b6de4daefbc1ece0c35aa2e0ee
nmbr7 <suhailr7@ymail.com>
2019-12-14 13:42:05 +0530

    updates

--- sys_setup.sh
+++ sys_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-sudo apt-get install -y vim tmux git curl build-essential tex* qemu*
+#sudo apt-get install -y vim tmux git curl build-essential tex* qemu*
 sudo cp .tmux.conf .vimrc $HOME/
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
-source $HOME/.bashrc
-echo -e 'Sourced bashrc -_- \n'
-curl https://sh.rustup.rs -sSf | sh
-echo 'PATH=$PATH:$HOME/.cargo/bin' >> /home/ubuntu/.bashrc
+mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && \
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cp -r ./colors $HOME/.vim/
+#git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
+#source $HOME/.bashrc
+#echo -e 'Sourced bashrc -_- \n'
+#curl https://sh.rustup.rs -sSf | sh
+#echo 'PATH=$PATH:$HOME/.cargo/bin' >> /home/ubuntu/.bashrc
 
